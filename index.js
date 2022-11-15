293273ca24b118b0269cb6dc9b5b97a12be31c6e
Zehedina Smajlovic <usatransport16@gmail.com>
2022-11-14 17:55:10 -0700

    commit tree af1d331dd45ce17c372220bc38b97c683d0f3838

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+
+fetch ('https://anapioficeandfire.com/api/books')
+.then(res => res.json())
+.then(json =>{
+  renderBooks(json)
+})
+return fetch('https://anapioficeandfire.com/api/books')
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
